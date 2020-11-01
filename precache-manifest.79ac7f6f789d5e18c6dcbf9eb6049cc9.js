25336841553f4946ff08adcd4bd32f53d42e0747
trugamr <6trugamr9@gmail.com>
2020-11-01 14:47:49 +0530

    Updates

---
+++ precache-manifest.79ac7f6f789d5e18c6dcbf9eb6049cc9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215038010402a18f443ecea6e073520d",
+    "url": "/mausam/index.html"
+  },
+  {
+    "revision": "bb3caca0a79368d10254",
+    "url": "/mausam/static/css/main.6c7cb164.chunk.css"
+  },
+  {
+    "revision": "f7e2c010e85aed25e180",
+    "url": "/mausam/static/js/2.c3cf4c65.chunk.js"
+  },
+  {
+    "revision": "0fe7a40f879a1fcdb8492c455146a123",
+    "url": "/mausam/static/js/2.c3cf4c65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb3caca0a79368d10254",
+    "url": "/mausam/static/js/main.ccff9c76.chunk.js"
+  },
+  {
+    "revision": "7204fe93508be551bcd9",
+    "url": "/mausam/static/js/runtime-main.769ae6ee.js"
+  },
+  {
+    "revision": "adbe34b493ff3258279913d4ff71f130",
+    "url": "/mausam/static/media/clear-day.adbe34b4.svg"
+  },
+  {
+    "revision": "9c2a0d8db99c4c04831bc81b6c95dfa1",
+    "url": "/mausam/static/media/clear-night.9c2a0d8d.svg"
+  },
+  {
+    "revision": "8d0ca55048371d0eff8582c89ccbcef4",
+    "url": "/mausam/static/media/cloudy.8d0ca550.svg"
+  },
+  {
+    "revision": "b742a618f852e38e24f462505c470d84",
+    "url": "/mausam/static/media/fog.b742a618.svg"
+  },
+  {
+    "revision": "0fd7cfa77afa1856a03e2c5b2ae0254f",
+    "url": "/mausam/static/media/partly-cloudy-day.0fd7cfa7.svg"
+  },
+  {
+    "revision": "a871d3be2fca3c4a9a26b023eb94e158",
+    "url": "/mausam/static/media/partly-cloudy-night.a871d3be.svg"
+  },
+  {
+    "revision": "2ba6cde76df65917d7d47b47152dd31f",
+    "url": "/mausam/static/media/rain.2ba6cde7.svg"
+  },
+  {
+    "revision": "f9ec606ea02f038e59795eb0cb6a9cb9",
+    "url": "/mausam/static/media/sleet.f9ec606e.svg"
+  },
+  {
+    "revision": "c2418c4918f72da74a31953e9b51f101",
+    "url": "/mausam/static/media/snow.c2418c49.svg"
+  },
+  {
+    "revision": "332c94485bfbe74746125104c3fd32f8",
+    "url": "/mausam/static/media/wind.332c9448.svg"
+  }
+]);
